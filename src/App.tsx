12bcf055d6e34d22774eee127ca3e06479fc180a
Alexandro <alexlmangabeira@gmail.com>
2022-08-27 11:45:11 -0300

    inicialização do projeto - Lista de Tarefas

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as C from "./App.styles";
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>
+          Lista de Tarefas - Todo List
+        </C.Header>
+      </C.Area>
+    </C.Container>
+  );
+}
+
+export default App;
